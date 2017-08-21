796a9cd96c82667afc141c5dfc3f15c57da3c1ab
iibi0107 <iibi0107@iibi0107-PC>
2017-08-21 11:40:45 +0900

    QNAController추가

---
+++ MyProject/src/main/java/com/qna/controller/QnAController.java
@@ -0,0 +1,14 @@
+package com.qna.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class QnAController {
+
+	@RequestMapping("/qna")
+	public String qna(){
+		return "qna";
+	}
+	
+}
